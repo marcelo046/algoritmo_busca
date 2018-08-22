11d6270380123de52e406aa25a20e4b34d184fdd
marcelo046 <marc3lo046@gmail.com>
2018-08-22 11:03:08 -0300

    created function to plot graphs

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import random
-size = 20
+import matplotlib.pyplot as plt
 
-# ============ funções ============
+# declarações
+size = 20
+x = []
+
+# comandos iniciais
+for i in range(1, size+1):
+    x.append(i)
+
+#  funções
 def gen_random_list():
     return random.sample(range(1, 101), size)
 
@@ -9,8 +17,19 @@ def gen_order_list():
     list = random.sample(range(1, 101), size)    #creating a list in range 0 to 100
     list.sort()    # ordering
     return list
-# ---------------------------------
 
+def gen_graph(list, title='plot of points'):
+    plt.cla()
+    plt.clf()
+    #plt.close()
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('points')
+    plt.plot( x , y , 'ro')
+    plt.show()
+
+
+# programa principal main
 list_numbers_any = gen_random_list()
 list_numbers_order = gen_order_list()
 
